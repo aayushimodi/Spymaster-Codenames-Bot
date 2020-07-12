b399d145dfe00cad8c082453bae3cf0fb52466d2
neilhazra <neilhazra001@gmail.com>
2020-07-12 13:47:12 -0700

    it workssss!!!

--- main.py
+++ main.py
@@ -1,37 +1,44 @@
 import nltk
 from nltk.corpus import wordnet as wn
 import numpy as np
+import pickle as pk
+import math
+from sklearn.neighbors import NearestNeighbors
 
 def main():
-    map = {}
-    with open('glove.6B.50d.txt', 'rb') as f:
-        for l in f:
-            line = l.decode().split()
-            map[line[0]] = np.array(line[1:]).astype(np.float)
+    #map = {}
+    #with open('glove.6B.50d.txt', 'rb') as f:
+    #    for l in f:
+    #        line = l.decode().split()
+    #        map[line[0]] = np.array(line[1:]).astype(np.float)
             #np.array takes in a list and returns a vector with the elements of the list
-    f.close()
+    with open('word2vec.pk', 'rb+') as f:
+        map = pk.load(f)
+    print("loaded")
+    #nbrs = NearestNeighbors(n_neighbors=5, algorithm='ball_tree', metric = angularDistance).fit(np.array(list(map.values())))
+    with open('knn.pk', 'rb+') as f:
+        nbrs = pk.load(f)
+    #with open('knn.pk', 'wb+') as f:
+    #    pk.dump(nbrs, f)
 
-
-
-    girl = map['girl']
-    boy = map['boy']
-    king = map['king']
-    queen = map['queen']
-    antelope = map['antelope']
-    elephant = map['elephant']
-
-    quasiQueen = king - boy + girl
-    print(cosineSimilarity(quasiQueen, queen)) # should be similar
-    print(cosineSimilarity(boy, queen)) # should be less similar
-    print(cosineSimilarity(antelope, girl)) # should not be similar
-    print(cosineSimilarity(antelope, elephant))
-    
-
-
-    #   print(map['dog'])
+    print("creating neighbors")
+    #print(map['dog'].reshape(1,-1).shape)
+    distances, indices = nbrs.kneighbors(map['boy'].reshape(1,-1))
+    print(indices)
+    for i in range(5):
+        print(list(map.keys())[indices[0][i]])
 
 def cosineSimilarity(a, b):
-    return a.dot(b)/(np.linalg.norm(a) * np.linalg.norm(b))
+    result = a.dot(b)/(np.linalg.norm(a) * np.linalg.norm(b))
+    if result > 1:
+        return 1
+    elif result < -1:
+        return -1
+    else:
+        return result
+
+def angularDistance(a, b):
+    return math.acos(cosineSimilarity(a,b))/math.pi
 
 
 main()
